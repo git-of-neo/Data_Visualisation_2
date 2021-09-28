53f7cfece7f6a29338a1762b6bda6efe57abfaff
Neo <64857033+git-of-neo@users.noreply.github.com>
2021-09-29 02:14:51 +0800

    Initial commit

---
+++ js/viz.js
@@ -0,0 +1,21 @@
+var disable_action = {actions: false};
+
+var map = "js/choropleth_map.vg.json";
+    vegaEmbed("#choropleth_map", map, disable_action).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var bump = "js/bump_chart.vg.json";
+    vegaEmbed("#bump_chart", bump, disable_action).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var dot = "js/dot_chart.vg.json";
+    vegaEmbed("#dot_chart", dot, disable_action).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var bar = "js/bar_chart.vg.json";
+    vegaEmbed("#bar_chart", bar, disable_action).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
